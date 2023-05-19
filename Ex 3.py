80d2c76336c89e6c96adab74425f57a29d045457
Alan Vasconi <129455785+Alan-Vasconi@users.noreply.github.com>
2023-05-18 21:17:56 -0300

    Add files via upload

---
+++ Ex 3.py
@@ -0,0 +1,7 @@
+a = float(input())
+b = float(input())
+
+res = (a + b) / 2
+
+# SAIDA:
+print(f"{res:.2f}")
---
+++ Ex 4.py
@@ -0,0 +1,8 @@
+nome = input()
+a = float(input())
+b = float(input())
+
+res = (a + b) / 2
+
+# SAIDA:
+print(f"{nome} obteve {res:.2f} de media")
