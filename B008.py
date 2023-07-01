1b86ad4602759110ccefcd82d1433120d5b9a7eb
Alan Vasconi <129455785+Alan-Vasconi@users.noreply.github.com>
2023-06-30 23:28:30 -0300

    Add files via upload

---
+++ B008.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia três números inteiros A, B e C e imprima o maior deles.
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b and a > c:
+    print(a)
+elif b > c:
+    print(b)
+else:
+    print(c)
