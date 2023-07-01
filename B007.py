7b8160c9f81545f03964485f2ef2cd5b0465db76
Alan Vasconi <129455785+Alan-Vasconi@users.noreply.github.com>
2023-06-30 23:26:56 -0300

    Add files via upload

---
+++ B007.py
@@ -0,0 +1,12 @@
+# Faça um algoritmo que leia 2 valores inteiros A e B. Coloque-os em ordem crescente (ou seja, ao final A deve armazenar o menor valor e B o maior valor). Utilize o modelo abaixo apresentado no final do exercício, modificando apenas o processamento
+
+a = int(input())
+b = int(input())
+
+if a > b:
+    print(b)
+    print(a)
+else:
+    print(a)
+    print(b)
+
