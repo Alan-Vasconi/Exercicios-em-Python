73286b24e98a9e773182469196036ca7dc8b9e23
Alan Vasconi <129455785+Alan-Vasconi@users.noreply.github.com>
2023-07-02 17:21:27 -0300

    Add files via upload

---
+++ C001.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um número inteiro positivo N e imprima todos os números de 1 até N.
+
+numero = int(input())
+contador = 1 
+
+while contador <= numero:
+    print(contador)
+    contador += 1
+    
